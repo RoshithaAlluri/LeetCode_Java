e39f41d720a73eb43338720f56fcd148b83e6bce
RoshithaAlluri <roshitha.alluri9@gmail.com>
2025-08-16 09:31:54 +0530

    Create TwoSum(1).java

---
+++ TwoSum(1).java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Solution {
+public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> dict=new HashMap<>();
+    int n=nums.length;
+    for(int i=0;i<n;i++){
+        int t=target - nums[i];
+        if(dict.containsKey(t)){
+            int [] res= new int[]{i, dict.get(t)};
+            return res;
+        }
+        else {
+            dict.put(nums[i], i);
+        }
+    }
+    return  new int[]{};
+}
+}
